2ebca0c39c44eefcb2b46be08f7d7d7ea73ae6bc
Christine Groux <christine.groux@polymtl.ca>
2023-01-19 20:59:41 -0500

    remove footer stuff

--- components/BaelFooter.vue
+++ components/BaelFooter.vue
@@ -32,31 +32,6 @@
           </ul>
         </div>
       </div>
-      <div
-        class="xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none"
-        :class="signupAboutSize"
-      >
-        <div class="item">
-          <div class="footer__heading xs-mb2">Deploy</div>
-
-          <a
-            href="https://app.netlify.com/start/deploy?repository=https://github.com/jake-101/bael-template"
-          >
-            <img
-              style="height:29px;width:auto;"
-              src="~/assets/deploy.svg"
-              title="Deploy to Netlify"
-            />
-          </a>
-        </div>
-      </div>
-      <div class="c-12 xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none">
-        <div class="item xs-text-6">
-          <a href="https://github.com/jake-101/bael-template">Bael</a>
-          <span class="small-txt" v-if="version">{{version}}</span>, An open source design by
-          <a href="https://jake101.com">jake101</a>
-        </div>
-      </div>
     </div>
   </footer>
 </template>
